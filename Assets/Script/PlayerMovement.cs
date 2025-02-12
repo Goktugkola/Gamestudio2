6c3df65d919bc0dea62e520ebe0e8069a376844a
Göktuğ Kola <goktugkola@gmail.com>
2025-02-12 13:40:29 +0300

    swing mekaniği ilk prototip

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class AdvancedFPSController : MonoBehaviour
+public class CompletePlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
-    private float mouseSensitivity;
     [SerializeField] private float walkSpeed = 7f;
     [SerializeField] private float sprintSpeed = 12f;
     [SerializeField] private float groundAcceleration = 15f;
@@ -13,24 +12,35 @@ public class AdvancedFPSController : MonoBehaviour
     [SerializeField] private float jumpForce = 8f;
     [SerializeField] private float gravity = 24f;
     [SerializeField] private float airControl = 0.3f;
-    [SerializeField] private float maxAirSpeedMultiplier = 1.2f;
+
+    [Header("Swinging Settings")]
+    [SerializeField] private float maxSwingDistance = 25f;
+    [SerializeField] private float springForce = 4.5f;
+    [SerializeField] private float damperForce = 7f;
+    [SerializeField] private float horizontalThrustForce = 200f;
+    [SerializeField] private float forwardThrustForce = 200f;
+    [SerializeField] private LayerMask swingMask;
 
     [Header("References")]
     [SerializeField] private Transform orientation;
     [SerializeField] private Transform playerCamera;
-    
+    [SerializeField] private Transform gunTip;
+    [SerializeField] private LineRenderer swingLine;
+
     private Rigidbody rb;
+    private SpringJoint swingJoint;
+    private Vector3 swingPoint;
+    private bool isGrounded;
+    private bool isSwinging;
+    private Vector3 moveDirection;
     private float xRotation;
     private float yRotation;
-    private bool isGrounded;
-    private Vector3 moveDirection;
 
     private void Awake()
     {
-        mouseSensitivity = gameObject.GetComponent<CameraController>().mouseSensitivity;
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
-        rb.freezeRotation = true;
+        swingLine.positionCount = 0;
     }
 
     private void Update()
@@ -38,18 +48,32 @@ public class AdvancedFPSController : MonoBehaviour
         HandleMouseLook();
         HandleJump();
         GroundCheck();
+        HandleSwingInput();
     }
 
     private void FixedUpdate()
     {
-        HandleMovement();
-        ApplyGravity();
+        if (isSwinging)
+        {
+            ApplySwingForces();
+        }
+        else
+        {
+            HandleMovement();
+            ApplyGravity();
+        }
     }
 
+    private void LateUpdate()
+    {
+        DrawSwingRope();
+    }
+
+    #region Core Movement
     private void HandleMouseLook()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * 100f;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * 100f;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
@@ -74,21 +98,88 @@ public class AdvancedFPSController : MonoBehaviour
             AirMove(wishDir);
         }
     }
+    #endregion
 
+    #region Swing Mechanics
+    private void HandleSwingInput()
+    {
+        if (Input.GetMouseButtonDown(0)) StartSwing();
+        if (Input.GetMouseButtonUp(0)) StopSwing();
+    }
+
+    private void StartSwing()
+    {
+        if (Physics.SphereCast(playerCamera.position,1f, playerCamera.forward, out RaycastHit hit, maxSwingDistance, swingMask))
+        {
+            isSwinging = true;
+            swingPoint = hit.point;
+            swingJoint = gameObject.AddComponent<SpringJoint>();
+            
+            swingJoint.autoConfigureConnectedAnchor = false;
+            swingJoint.connectedAnchor = swingPoint;
+            
+            float distanceFromPoint = Vector3.Distance(transform.position, swingPoint);
+            swingJoint.maxDistance = distanceFromPoint * 0.8f;
+            swingJoint.minDistance = distanceFromPoint * 0.25f;
+            
+            swingJoint.spring = springForce;
+            swingJoint.damper = damperForce;
+            swingJoint.massScale = 4.5f;
+
+            swingLine.positionCount = 2;
+        }
+    }
+
+    private void StopSwing()
+    {
+        isSwinging = false;
+        swingLine.positionCount = 0;
+        Destroy(swingJoint);
+    }
+
+    private void ApplySwingForces()
+    {
+        // Horizontal movement
+        if (Input.GetKey(KeyCode.D)) rb.AddForce(orientation.right * horizontalThrustForce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A)) rb.AddForce(-orientation.right * horizontalThrustForce * Time.deltaTime);
+
+        // Forward movement
+        if (Input.GetKey(KeyCode.W)) rb.AddForce(orientation.forward * forwardThrustForce * Time.deltaTime);
+
+        // Shorten cable
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 directionToPoint = swingPoint - transform.position;
+            rb.AddForce(directionToPoint.normalized * forwardThrustForce * Time.deltaTime);
+            
+            float distanceFromPoint = Vector3.Distance(transform.position, swingPoint);
+            swingJoint.maxDistance = distanceFromPoint * 0.8f;
+            swingJoint.minDistance = distanceFromPoint * 0.25f;
+        }
+    }
+
+    private void DrawSwingRope()
+    {
+        if (!isSwinging) return;
+
+        swingLine.SetPosition(0, gunTip.position);
+        swingLine.SetPosition(1, swingPoint);
+    }
+    #endregion
+
+    #region Movement Helpers
     private void GroundMove(Vector3 wishDir)
     {
         float currentSpeed = Vector3.Dot(rb.linearVelocity, wishDir);
         float addSpeed = Mathf.Clamp(walkSpeed - currentSpeed, 0, walkSpeed);
-        
         rb.AddForce(wishDir * (groundAcceleration * addSpeed * Time.fixedDeltaTime), ForceMode.Acceleration);
     }
 
     private void AirMove(Vector3 wishDir)
     {
-        // Air strafing - preserve momentum while allowing directional control
         float wishSpeed = airAcceleration * Time.fixedDeltaTime;
         float currentSpeed = Vector3.Dot(rb.linearVelocity, wishDir);
-        float addSpeed = Mathf.Clamp(walkSpeed * maxAirSpeedMultiplier - currentSpeed, 0, wishSpeed);
+        float addSpeed = Mathf.Clamp(walkSpeed * 1.2f - currentSpeed, 0, wishSpeed);
 
         if (addSpeed > 0)
         {
@@ -110,7 +201,7 @@ public class AdvancedFPSController : MonoBehaviour
 
     private void ApplyGravity()
     {
-        if (!isGrounded)
+        if (!isGrounded && !isSwinging)
         {
             rb.AddForce(Vector3.down * gravity * rb.mass, ForceMode.Force);
         }
@@ -126,7 +217,20 @@ public class AdvancedFPSController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
+        }
+    }
+    #endregion
+    private void OnDrawGizmos()
+    {
+        if (isSwinging)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(swingPoint, 1f);
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(playerCamera.position + playerCamera.forward * maxSwingDistance, 1f);
         }
     }
 }
