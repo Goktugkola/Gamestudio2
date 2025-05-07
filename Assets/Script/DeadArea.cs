352eb20d14bb566678d8eeb98d69cb8b65ad9594
Göktuğ Kola <goktugkola@gmail.com>
2025-05-07 18:43:14 +0300

    deadarea update

--- Assets/Script/DeadArea.cs
+++ Assets/Script/DeadArea.cs
@@ -11,8 +11,12 @@ public class DeadArea : MonoBehaviour
             print(other.GetComponentInParent<PlayerInteraction>().gameObject.GetComponentInChildren<Camera>().gameObject.name);
             print(other.GetComponentInParent<PlayerMovement>().gameObject.name);
 
-            other.GetComponentInParent<PlayerInteraction>().gameObject.GetComponentInChildren<Camera>().transform.position = CheckPoint.transform.position;
-            other.GetComponentInParent<PlayerMovement>().gameObject.transform.position = CheckPoint.transform.position;
+            
+            other.transform.parent.transform.parent.GetComponentInChildren<Camera>().transform.position = CheckPoint.transform.position;
+            other.transform.parent.transform.parent.GetComponentInChildren<Camera>().transform.rotation = CheckPoint.transform.rotation;
+            other.transform.parent.transform.position = CheckPoint.transform.position;
+            other.transform.parent.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+
         }
     }
 }
