43a04cda9f69f183646d62d95bff6f756016abf3
Göktuğ Kola <goktugkola@gmail.com>
2025-05-08 23:45:34 +0300

    target prefab

--- Assets/Script/obstacle.cs
+++ Assets/Script/obstacle.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class obstacle : MonoBehaviour
 {
     Animation hitanim;
+    bool animEnded = false;
+    void Start()
+    {
+        hitanim = GetComponent<Animation>();
+        if (hitanim == null)
+        {
+            Debug.LogError("Animation component not found on the object.", this);
+        }
+    }
     void Hit()
     {
         if(hitanim != null)
         {
-            hitanim.Play("HitAnimation"); // Replace with your animation name
+            hitanim.Play("Target"); // Replace with your animation name
         }
     }
 }
