a5ca9faade6afd37ba320921ca55f4427d49ec06
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2025-05-13 18:57:36 +0300

    Pause menu tamam kardşşş

--- Assets/Script/MenuScreen.cs
+++ Assets/Script/MenuScreen.cs
@@ -5,6 +5,8 @@ public class MenuScreen : MonoBehaviour
 {
     [SerializeField] private bool isMenuActive = false;
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private CamFollow camFollowScript1;
+    [SerializeField] private CamFollow camFollowScript2;
     [SerializeField] private GameObject menuScreen; // Assign in Inspector
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,25 +19,42 @@ public class MenuScreen : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("Escye girür mü");
             ToggleMenu();
         }
     }
     public void ToggleMenu()
     {
           isMenuActive = !isMenuActive;
-    menuScreen.SetActive(isMenuActive);
+          menuScreen.SetActive(isMenuActive);
 
     if (isMenuActive)
     {
         Time.timeScale = 0; // Pause the game
         Cursor.lockState = CursorLockMode.None; // Unlock the cursor
         Cursor.visible = true; // Make the cursor visible
+        if(camFollowScript1 != null)
+        {
+            camFollowScript1.enabled = false; // Disable the camera follow script
+        }
+        if(camFollowScript2 != null)
+        {
+            camFollowScript2.enabled = false; // Disable the camera follow script
+        }
     }
     else
     {
         Time.timeScale = 1; // Resume the game
         Cursor.lockState = CursorLockMode.Locked; // Lock the cursor
         Cursor.visible = false; // Hide the cursor
+        if(camFollowScript1 != null)
+        {
+            camFollowScript1.enabled = true; // Disable the camera follow script
+        }
+        if(camFollowScript2 != null)
+        {
+            camFollowScript2.enabled = true; // Disable the camera follow script
+        }
     }
     }
     public void QuitGame()
